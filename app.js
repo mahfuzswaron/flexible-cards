d6c6785c236fa3451862bd38858ca4478ad69bea
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-25 09:52:53 +0600

    finalized

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const layout1 = document.getElementById("layout-1");
 const layout2 = document.getElementById("layout-2");
 const layout3 = document.getElementById("layout-3");
-const cards = layout1.children;
 const middleDiv = document.getElementById("middle-div");
+const layout1Cards = layout1.children;
+const layout2Cards = layout2.children;
+const layout3Cards = layout3.children;
+const middleDivCards = middleDiv.children; // in layout 3
+
+// layout tab swithcing fuctions
 function displayLayout1() {
     layout1.style.display = "grid";
     layout2.style.display = "none";
@@ -19,22 +24,28 @@ function displayLayout3() {
     layout3.style.display = "grid";
 }
 
-function animateCards() {
+// fucntion for animating the cards
+
+(function () {
+
+    // adds duration to all cards
+    const allCards = document.getElementsByClassName("card")
+    for (const card of allCards) {
+        card.style.setProperty('--animate-duration', '2s');
+    }
 
     // animates layout 1
-    for (let i = 0; i < cards.length; i++) {
-        const card = cards[i];
-        card.style.setProperty('--animate-duration', '2s');
+    for (let i = 0; i < layout1Cards.length; i++) {
+        const card = layout1Cards[i];
         if (i % 2 === 0) {
             card.classList.add("animate__animated", "animate__slideInLeft")
         }
         else {
             card.classList.add("animate__animated", "animate__slideInRight")
         }
-
     }
+
     // animates layout 2
-    const layout2Cards = layout2.children;
     for (let i = 0; i < layout2Cards.length; i++) {
         const card = layout2Cards[i];
         if (i < 3) {
@@ -43,31 +54,20 @@ function animateCards() {
         else {
             card.classList.add("animate__animated", "animate__slideInRight")
         }
-        card.style.setProperty('--animate-duration', '2s');
-
     }
 
     // animates 3 cards in middle div in layout 3 
-    const middleDivCards = middleDiv.children;
     for (let i = 0; i < middleDivCards.length; i++) {
         const card = middleDivCards[i];
-        card.classList.add("animate__animated", "animate__slideInRight")
-
+        card.classList.add("animate__animated", "animate__slideInRight");
     }
 
     // animates rest 2 elements in layout 3
-    const layout3Cards = layout3.children;
     for (let i = 0; i < layout3Cards.length; i++) {
         const card = layout3Cards[i];
         if (card.id !== "middle-div") {
             card.classList.add("animate__animated", "animate__slideInLeft")
-            card.style.setProperty('--animate-duration', '2s');
         }
     }
 
-
-}
-
-animateCards()
-
-// 
+})()
