decc28d325f8d45bd991ca2b3639ab2cc412b0c1
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-25 09:01:35 +0600

    layout 1 animation done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const layout1 = document.getElementById("layout-1");
 const layout2 = document.getElementById("layout-2");
 const layout3 = document.getElementById("layout-3");
-
+const cards = layout1.children;
+const middleDiv = document.getElementById("middle-div");
 function displayLayout1() {
     layout1.style.display = "grid";
     layout2.style.display = "none";
@@ -17,3 +18,35 @@ function displayLayout3() {
     layout2.style.display = "none";
     layout3.style.display = "grid";
 }
+
+function animateCards() {
+
+    // animates all cards in layout 1
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        card.style.setProperty('--animate-duration', '2s');
+        if (i % 2 === 0) {
+            card.classList.add("animate__animated", "animate__slideInLeft")
+        }
+        else {
+            card.classList.add("animate__animated", "animate__slideInRight")
+        }
+
+    }
+
+    // animates 3 cards in middle div in layout 3 
+    const middleDivCards = middleDiv.children;
+    for (let i = 0; i < middleDivCards.length; i++) {
+        const card = middleDivCards[i];
+        card.classList.add("animate__animated", "animate__slideInRight")
+
+    }
+
+    // animates 
+
+
+}
+
+animateCards()
+
+// 
